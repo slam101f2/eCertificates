4f0725fb4a2d3bb3f86ed265e611558e74d8e8b6
slam101f2 <73736167+slam101f2@users.noreply.github.com>
2020-10-31 13:43:17 +0530

    Update admin.js

---
+++ controllers/admin.js
@@ -0,0 +1,10 @@
+// Login controller
+// view citizen controller
+// verify citizen controller
+// create certificate controller
+// manage citizen controller
+// upload document controller
+// create citizen profile controller
+// check for new citizen controller
+// issue card controller
+
